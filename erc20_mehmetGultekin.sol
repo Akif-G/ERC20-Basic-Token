93a43fa94c8f8dc5a913d552bd38766005d3490d
Akif Gultekin <mehmetgultekin@sabanciuniv.edu>
2020-05-19 02:48:56 +0300

    Initial erc20 implementation is set.

---
+++ erc20_mehmetGultekin.sol
@@ -0,0 +1,26 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+contract MehmetGultekin{
+
+event Transfer(address indexed _from, address indexed _to, uint256 _value);
+event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+function name() public view returns (string memory){}
+
+function symbol() public view returns (string memory){}
+
+function decimals() public view returns (uint8){}
+
+function totalSupply() public view returns (uint256){}
+
+function balanceOf(address _owner) public view returns (uint256 balance){}
+
+function transfer(address _to, uint256 _value) public returns (bool success){}
+
+function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){}
+
+function approve(address _spender, uint256 _value) public returns (bool success){}
+
+function allowance(address _owner, address _spender) public view returns (uint256 remaining){}
+
+}
