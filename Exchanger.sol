b12d9f26ca6036d20a64c04d4ef3fe39c46e87a5
Akif Gultekin <mehmetgultekin@sabanciuniv.edu>
2020-05-19 06:20:14 +0300

    implementation ends except exchanger.sol

--- Exchanger.sol
+++ Exchanger.sol
@@ -8,6 +8,31 @@ pragma solidity >=0.4.22 <0.7.0;
  *          https://github.com/Akif-G/ERC20-Basic-Token.git
  */
 
+///import "erc20_GultekinMehmet.sol";
+///import "erc20_MehmetGultekin.sol";
+import "erc20_TemplateToken.sol";
+
 contract Exchanger{
-    function transfer(address from_addrs, address to_addrs, uint256 amount) public{}
+    address nftAddress = address(this);
+    address payable _wallet = address(uint160(nftAddress));
+
+    function getExchangeAddress()public view returns(address){
+        return _wallet;
+    }
+
+
+    function transfer(address from_addrs, address to_addrs, uint256 amount) public returns(bool success){
+        MehmetGultekinToken first = MehmetGultekinToken(from_addrs);
+        MehmetGultekinToken second = MehmetGultekinToken(to_addrs);
+        assert(amount>0);
+        require(first.balanceOf(msg.sender)>=amount, "not enough token to Exchange");
+        require(second.balanceOf(msg.sender)>=amount, "not enough token to Exchange");
+        require(first.allowance(msg.sender,_wallet)>=amount, "tokens are not allowed to use");
+        require(second.allowance(msg.sender,_wallet)>=amount, "tokens are not allowed to use");
+        
+        
+        
+        return true;
+
+    }
 }
---
+++ erc20_TemplateToken.sol
@@ -0,0 +1,77 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+/**
+ * @title   Standard ERC20 token
+ * @author  github.com/Akif-G
+ * @notice  different Name, but the same implementation with:: github.com/Akif-G/erc20_MehmetGultekin.sol
+ */
+
+contract MehmetGultekinToken{
+
+    mapping (address => uint256) private _balances;
+    mapping (address => mapping (address => uint256)) private _allowed;
+    uint256 private _totalSupply;
+    string private _name;
+    string private _symbol;
+
+    constructor(uint256 _total, string memory name, string memory symbol) public {
+        assert(_total>0);
+       _totalSupply = _total;
+       _name = name;
+       _symbol = symbol;
+       _balances[msg.sender] = _totalSupply;
+    }
+
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+
+    function name() public view returns (string memory){
+        return _name;
+    }
+
+    function symbol() public view returns (string memory){
+        return _symbol;
+    }
+
+    function totalSupply() public view returns (uint256){
+        return _totalSupply;
+    }
+
+    function balanceOf(address _owner) public view returns (uint256 balance){
+        return _balances[_owner];
+    }
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining){
+        return _allowed[_owner][_spender];
+    }
+
+    function transfer(address _to, uint256 _value) public returns (bool success){
+        assert(_value>0);
+        require(_value <= _balances[msg.sender],"Needs a positive input");
+        require(_to != address(0),"Provide an address");
+
+        _balances[msg.sender] = _balances[msg.sender] - (_value);
+        emit Transfer(msg.sender, _to, _value);
+        _balances[_to] = _balances[_to] + (_value);
+        return true;
+  }
+
+    function transferFrom( address from, address _to, uint256 _value) public returns (bool){
+        require(_value <= _balances[from],"Not enough token");
+        require(_value <= _allowed[from][msg.sender], "Not allowed by owner");
+        require(_to != address(0), "Provide an address");
+
+        _balances[from] = _balances[from] - (_value);
+        _balances[_to] = _balances[_to] + (_value);
+        _allowed[from][msg.sender] = _allowed[from][msg.sender] - (_value);
+        emit Transfer(from, _to, _value);
+        return true;
+    }
+
+    function approve(address _delegate, uint256 _value) public returns (bool success){
+        _allowed[msg.sender][_delegate] = _value;
+        emit Approval(msg.sender, _delegate, _value);
+        return true;
+    }
+
+}
