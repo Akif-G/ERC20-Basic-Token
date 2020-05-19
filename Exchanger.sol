b2a70ae8fe3e2e0d22796e377b13cb938f9c9fed
Akif Gultekin <mehmetgultekin@sabanciuniv.edu>
2020-05-19 05:07:06 +0300

    Second ERC20 token is created, replicated the first one. Exchanger contract is designed with only one function. gitignore folder added.

---
+++ Exchanger.sol
@@ -0,0 +1,13 @@
+pragma solidity >=0.4.22 <0.7.0;
+
+/**
+ * @title   Standard ERC20 token Exchanger
+ * @author  github.com/Akif-G
+ * @notice  Basic ERC20 token Exchange mechanism implementation, Not recommended for commercial usage.
+ * @dev     Implementation of the basic standard token, can be used for referance without guarranty.
+ *          https://github.com/Akif-G/ERC20-Basic-Token.git
+ */
+
+contract Exchanger{
+    function transfer(address from_addrs, address to_addrs, uint256 amount) public{}
+}
