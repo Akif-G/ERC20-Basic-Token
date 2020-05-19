3cc155d515cbc22e701ef8af6e3200c20c639575
Akif Gultekin <mehmetgultekin@sabanciuniv.edu>
2020-05-19 03:16:53 +0300

    natspec formation + initial.

--- erc20_mehmetGultekin.sol
+++ erc20_mehmetGultekin.sol
@@ -1,26 +1,36 @@
 pragma solidity >=0.4.22 <0.7.0;
 
+/**
+ * @title   Standard ERC20 token
+ * @author  github.com/Akif-G
+ * @notice  Basic ERC20 token implementation, Not recommended for commercial usage.
+ * @dev     Implementation of the basic standard Ethereum token, can be used for referance without any guarantee.
+ *          https://github.com/Akif-G/ERC20-Basic-Token.git
+ */
 contract MehmetGultekin{
 
-event Transfer(address indexed _from, address indexed _to, uint256 _value);
-event Approval(address indexed _owner, address indexed _spender, uint256 _value);
+    mapping (address => uint256) private _balances;
 
-function name() public view returns (string memory){}
+    mapping (address => mapping (address => uint256)) private _allowed;
+    event Transfer(address indexed _from, address indexed _to, uint256 _value);
+    event Approval(address indexed _owner, address indexed _spender, uint256 _value);
 
-function symbol() public view returns (string memory){}
+    function name() public view returns (string memory){}
 
-function decimals() public view returns (uint8){}
+    function symbol() public view returns (string memory){}
 
-function totalSupply() public view returns (uint256){}
+    function decimals() public view returns (uint8){}
 
-function balanceOf(address _owner) public view returns (uint256 balance){}
+    function totalSupply() public view returns (uint256){}
 
-function transfer(address _to, uint256 _value) public returns (bool success){}
+    function balanceOf(address _owner) public view returns (uint256 balance){}
 
-function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){}
+    function transfer(address _to, uint256 _value) public returns (bool success){}
 
-function approve(address _spender, uint256 _value) public returns (bool success){}
+    function transferFrom(address _from, address _to, uint256 _value) public returns (bool success){}
 
-function allowance(address _owner, address _spender) public view returns (uint256 remaining){}
+    function approve(address _spender, uint256 _value) public returns (bool success){}
+
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining){}
 
 }
